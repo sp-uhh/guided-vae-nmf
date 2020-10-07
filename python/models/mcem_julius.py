corrected mcem_julius (pb with sqrt(var)) + corrected waveplot scaling in visualization

python/models/mcem_julius.py
@@ -12,14 +12,14 @@ class MCEM_M2:
         eps = np.finfo(float).eps
         self.K = NMF_rank
         np.random.seed(0)
-        self.W = np.array(np.maximum(np.random.rand(self.F,self.K), eps))
-        self.H = np.array(np.maximum(np.random.rand(self.K,self.T), eps))
+        self.W = np.array(np.maximum(np.random.rand(self.F,self.K), self.eps), dtype='float32')
+        self.H = np.array(np.maximum(np.random.rand(self.K,self.T), self.eps), dtype='float32')
         self.V = np.matmul(self.W,self.H) 
         self.Z = Z
         self.y = y
         self.D = self.Z.shape[0]
         self.model = model
-        self.g = np.ones((1,self.T)) 
+        self.g = np.ones((1,self.T), dtype='float32') 
         self.niter_MH = niter_MH 
         self.niter_MCEM = niter_MCEM
         self.burnin = burnin 
@@ -31,7 +31,7 @@ class MCEM_M2:
         Z_sampled = torch.zeros((self.D, self.T, niter_MH - burnin))
         cpt = 0
         for n in range(niter_MH):
-            Z_prime = self.Z + torch.tensor(self.var_MH)*torch.randn(self.D,self.T).to(self.device) 
+            Z_prime = self.Z + np.sqrt(self.var_MH)*torch.randn(self.D,self.T).to(self.device) 
  
             #S_hat_prime = self.model.decode(Z_prime).cpu().numpy()     
             S_hat_prime = self.model.decoder(torch.t(torch.cat([Z_prime, self.y],axis=0))).cpu().numpy().T
@@ -136,13 +136,13 @@ class MCEM_M1:
         self.eps = eps
         self.K = NMF_rank
         np.random.seed(0)
-        self.W = np.array(np.maximum(np.random.rand(self.F,self.K), self.eps))
-        self.H = np.array(np.maximum(np.random.rand(self.K,self.T), self.eps))
+        self.W = np.array(np.maximum(np.random.rand(self.F,self.K), self.eps), dtype='float32')
+        self.H = np.array(np.maximum(np.random.rand(self.K,self.T), self.eps), dtype='float32')
         self.V = np.matmul(self.W,self.H) 
         self.Z = Z
         self.D = self.Z.shape[0]
         self.model = model
-        self.g = np.ones((1,self.T)) 
+        self.g = np.ones((1,self.T), dtype='float32') 
         self.niter_MH = niter_MH 
         self.niter_MCEM = niter_MCEM
         self.burnin = burnin 
@@ -154,7 +154,8 @@ class MCEM_M1:
         Z_sampled = torch.zeros((self.D, self.T, niter_MH - burnin))
         cpt = 0
         for n in range(niter_MH):
-            Z_prime = self.Z + torch.tensor(self.var_MH)*torch.randn(self.D,self.T).to(self.device) 
+            #Z_prime = self.Z + torch.tensor(self.var_MH)*torch.randn(self.D,self.T).to(self.device) 
+            Z_prime = self.Z + np.sqrt(self.var_MH)*torch.randn(self.D,self.T).to(self.device) 
  
             S_hat_prime = self.model.decoder(torch.t(Z_prime)).cpu().numpy().T
     
@@ -164,7 +165,7 @@ class MCEM_M1:
                 + (1/(self.V + self.speech_var) - 1/(self.V + speech_var_prime))* np.abs(self.X)**2, axis=0)
                 + .5*np.sum(self.Z.cpu().numpy()**2 - Z_prime.cpu().numpy()**2, axis=0))
             
-            is_acc = np.log(np.random.rand(self.T)) < acc_prob
+            is_acc = np.log(np.random.rand(self.T).astype('float32')) < acc_prob
                         
             self.Z[:,is_acc] = Z_prime[:,is_acc]
             self.Z_mapped_decoder = self.model.decoder(torch.t(self.Z)).cpu().numpy().T

python/models/mcem_simon.py
@@ -340,7 +340,7 @@ class MCEM_M1(EM):
         
         
         
-    def sample_posterior(self, Z, y, nsamples=10, burnin=30):
+    def sample_posterior(self, Z, nsamples=10, burnin=30):
         # Metropolis-Hastings
         
         F, N = self.X.shape
@@ -349,6 +349,8 @@ class MCEM_M1(EM):
             L = self.vae.latent_dim
         elif hasattr(self.vae, 'z_dim'):
             L = self.vae.z_dim
+        else:
+            L = Z.shape[0]
         
         # random walk variance as tensor
         #var_RM_t = torch.tensor(np.float32(self.var_RW))

python/visualization.py
@@ -7,6 +7,8 @@ import matplotlib.gridspec as grd
 
 def display_waveplot(x,
                      fs=16e3,
+                     ymax=1.,
+                     ymin=-1.,
                      xticks_sec=1.0,
                      fontsize=50):
     """
@@ -35,6 +37,7 @@ def display_waveplot(x,
     plt.xlabel('Time (s)', fontsize=fontsize+10) #, fontweight="bold")
     plt.xticks(np.arange(0, time_sec, step=xticks_sec), fontsize=fontsize)
     plt.yticks(fontsize=fontsize)
+    plt.ylim(ymin=ymin, ymax=ymax)
 
     return img
 
@@ -168,7 +171,7 @@ def display_wav_spectro_mask(x,
 
     # line plot
     ax = plt.subplot(gs[0])
-    display_waveplot(x, fs, xticks_sec, fontsize)
+    display_waveplot(x=x, fs=fs, xticks_sec=xticks_sec, fontsize=fontsize)
 
     # image plot
     ax = plt.subplot(gs[2])
@@ -229,7 +232,7 @@ def display_multiple_signals(signal_list,
 
         # line plot
         ax = plt.subplot(gs[3*i])
-        display_waveplot(x, fs, xticks_sec, fontsize)
+        display_waveplot(x=x, fs=fs, xticks_sec=xticks_sec, fontsize=fontsize)
 
         # image plot
         #ax = plt.subplot(gs[(i+2)])
@@ -298,7 +301,7 @@ def display_multiple_spectro(signal_list,
         if not (x is None):
             # line plot
             ax = plt.subplot(gs[3*i])
-            display_waveplot(x, fs, xticks_sec, fontsize)
+            display_waveplot(x=x, fs=fs, xticks_sec=xticks_sec, fontsize=fontsize)
 
         # image plot
         #ax = plt.subplot(gs[(i+2)])

scripts/evaluate_M1.py
@@ -49,10 +49,32 @@ dtype = 'complex64'
 # z_dim = 128
 # h_dim = [256, 128]
 
-model_name = 'M1_h128_end_epoch_050/M1_epoch_044_vloss_486.29'
+# model_name = 'M1_h128_end_epoch_050/M1_epoch_044_vloss_486.29'
+# x_dim = 513 # frequency bins (spectrogram)
+# z_dim = 128
+# h_dim = [128]
+
+# model_name = 'M1_h128_z16_end_epoch_250/M1_epoch_010_vloss_526.58'
+# x_dim = 513 # frequency bins (spectrogram)
+# z_dim = 16
+# h_dim = [128]
+
+# model_name = 'M1_h128_z032_end_epoch_250/M1_epoch_200_vloss_467.83'
+# x_dim = 513 # frequency bins (spectrogram)
+# z_dim = 32
+# h_dim = [128]
+
+# model_name = 'M1_ISv2_eps1e-5_h128_z032_end_epoch_250/M1_epoch_200_vloss_434.08'
+# x_dim = 513 # frequency bins (spectrogram)
+# z_dim = 32
+# h_dim = [128]
+# eps = 1e-5
+
+model_name = 'M1_ISv2_h128_z032_end_epoch_250/M1_epoch_200_vloss_467.95'
 x_dim = 513 # frequency bins (spectrogram)
-z_dim = 128
+z_dim = 32
 h_dim = [128]
+eps = 1e-8
 
 ## Monte-Carlo EM
 use_mcem_julius = False
@@ -86,7 +108,7 @@ def main():
     test_data = pickle.load(open(os.path.join('data', dataset_size, 'pickle/si_et_05_mixture-505.p'), 'rb'))
 
     model = VariationalAutoencoder([x_dim, z_dim, h_dim])
-    model.load_state_dict(torch.load(os.path.join('models', model_name + '.pt')))
+    model.load_state_dict(torch.load(os.path.join('models', model_name + '.pt'), map_location="cuda:0"))
     if cuda: model = model.cuda()
 
     model.eval()
@@ -151,9 +173,9 @@ def main():
 
             # NMF parameters are initialized outside MCEM
             N, F = x_tf.shape
-            W_init = np.maximum(np.random.rand(F,nmf_rank), eps)
-            H_init = np.maximum(np.random.rand(nmf_rank, N), eps)
-            g_init = torch.ones(N).to(device)
+            W_init = np.maximum(np.random.rand(F,nmf_rank), eps, dtype='float32')
+            H_init = np.maximum(np.random.rand(nmf_rank, N), eps, dtype='float32')
+            g_init = torch.ones(N).to(device) # float32 by default
 
             mcem = mcem_simon.MCEM_M1(X=x_tf,
                                 W=W_init,

scripts/evaluate_M2.py
@@ -163,9 +163,9 @@ def main():
 
             # NMF parameters are initialized outside MCEM
             N, F = x_tf.shape
-            W_init = np.maximum(np.random.rand(F,nmf_rank), eps)
-            H_init = np.maximum(np.random.rand(nmf_rank, N), eps)
-            g_init = torch.ones(N).to(device)
+            W_init = np.maximum(np.random.rand(F,nmf_rank), eps, dtype='float32')
+            H_init = np.maximum(np.random.rand(nmf_rank, N), eps, dtype='float32')
+            g_init = torch.ones(N).to(device) # float32 by default
 
             mcem = mcem_simon.MCEM_M2(X=x_tf,
                                 W=W_init,
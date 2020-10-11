bcfe3230818792a875cbb0ad7f3d5f6dabccfcef
Guillaume Carbajal <carbajal@informatik.uni-hamburg.de>
2020-10-11 18:57:37 +0200

    cleaned visualization.py

--- python/visualization.py
+++ python/visualization.py
@@ -198,50 +198,6 @@ def display_wav_spectro_mask(x,
 
     return fig
 
-def display_wav_spectro_ma(x,
-                             x_tf,
-                             x_ibm,
-                             fs=16e3,
-                             vmin=-60,
-                             vmax=10,
-                             wlen_sec=50e-3,
-                             hop_percent=0.5,
-                             xticks_sec=1.0,
-                             fontsize=50):
-        
-    # Plot waveplot + spectrogram + binary mask
-    fig = plt.figure(figsize=(20,25))
-
-    # create a 2 X 2 grid
-    gs = grd.GridSpec(3, 2,
-                    height_ratios=[5,10,10],
-                    width_ratios=[10,0.5],
-                    wspace=0.1,
-                    hspace=0.3,
-                    left=0.08)
-
-    # line plot
-    ax = plt.subplot(gs[0])
-    display_waveplot(x=x, fs=fs, xticks_sec=xticks_sec, fontsize=fontsize)
-
-    # image plot
-    ax = plt.subplot(gs[2])
-    display_spectrogram(x_tf, True, fs, vmin, vmax, wlen_sec, hop_percent, xticks_sec, 'magma', fontsize)
-
-    # color bar in it's own axis
-    colorAx = plt.subplot(gs[3])
-    cbar = plt.colorbar(cax=colorAx, format='%+2.0f dB')
-
-    # image plot
-    ax = plt.subplot(gs[4])
-    display_spectrogram(x_ibm, False, fs, 0, 1, wlen_sec, hop_percent, xticks_sec, 'Greys_r', fontsize)
-
-    # color bar in it's own axis
-    colorAx = plt.subplot(gs[5])
-    plt.colorbar(cax=colorAx, format='%0.1f')
-
-    return fig
-
 def display_multiple_signals(signal_list,
                              fs=16e3,
                              vmin=-60,

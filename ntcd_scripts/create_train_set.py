513150ae34e7293f4ffa15299ab9ca17adaeb041
Guillaume Carbajal <carbajal@informatik.uni-hamburg.de>
2020-10-19 16:39:34 +0200

    noise-robust VAD and IBM in create_train_set for ntcd-timit

--- ntcd_scripts/create_train_set.py
+++ ntcd_scripts/create_train_set.py
@@ -9,7 +9,7 @@ import math
 
 from python.dataset.ntcd_timit_dataset import speech_list, write_dataset
 from python.processing.stft import stft
-from python.processing.target import clean_speech_IBM, clean_speech_VAD
+from python.processing.target import noise_robust_clean_speech_IBM, noise_robust_clean_speech_VAD
 from python.utils import open_file
 
 
@@ -17,8 +17,8 @@ from python.utils import open_file
 ## Dataset
 dataset_types = ['train', 'validation']
 
-# dataset_size = 'subset'
-dataset_size = 'complete'
+dataset_size = 'subset'
+# dataset_size = 'complete'
 
 input_speech_dir = os.path.join('data', dataset_size, 'raw/')
 output_pickle_dir = os.path.join('data', dataset_size, 'pickle/')
@@ -34,8 +34,13 @@ pad_mode = 'reflect' # This argument is ignored if center = False
 pad_at_end = True # pad audio file at end to match same size after stft + istft
 dtype = 'complex64'
 
-## Ideal binary mask
-quantile_fraction = 0.999
+## Noise robust VAD
+vad_quantile_fraction_begin = 0.93
+vad_quantile_fraction_end = 0.99
+quantile_weight = 0.999
+
+## Noise robust IBM
+ibm_quantile_fraction = 0.999
 quantile_weight = 0.999
 
 def main():
@@ -74,21 +79,24 @@ def main():
                                  dtype=dtype) # shape = (freq_bins, frames)
 
                 # # binary mask
-                # speech_ibm = clean_speech_IBM(speech_tf,
-                #                         quantile_fraction=quantile_fraction,
-                #                         quantile_weight=quantile_weight)
+                # speech_ibm = noise_robust_clean_speech_IBM(speech_tf,
+                #                                     vad_quantile_fraction_begin=vad_quantile_fraction_begin,
+                #                                     vad_quantile_fraction_end=vad_quantile_fraction_end,
+                #                                     ibm_quantile_fraction=ibm_quantile_fraction,
+                #                                     quantile_weight=quantile_weight)
                                     
                 # vad
-                speech_vad = clean_speech_VAD(speech_tf,
-                                        quantile_fraction=quantile_fraction,
-                                        quantile_weight=quantile_weight)
+                speech_vad = noise_robust_clean_speech_VAD(speech_tf,
+                                                    quantile_fraction_begin=vad_quantile_fraction_begin,
+                                                    quantile_fraction_end=vad_quantile_fraction_end,
+                                                    quantile_weight=quantile_weight)
                 
                 if iteration == 0:
                     # labels.append(speech_ibm)
                     labels.append(speech_vad)
 
-                # if iteration == 1:
-                #     spectrograms.append(np.power(abs(speech_tf), 2))
+                if iteration == 1:
+                    spectrograms.append(np.power(abs(speech_tf), 2))
 
             if iteration == 0:
                 labels = np.concatenate(labels, axis=1)
@@ -107,15 +115,15 @@ def main():
 
                 del labels            
 
-            # if iteration == 1:          
-            #     spectrograms = np.concatenate(spectrograms, axis=1)
-            #     # write spectrograms
-            #     write_dataset(spectrograms,
-            #                 output_data_dir=output_pickle_dir,
-            #                 dataset_type=dataset_type,
-            #                 suffix='frames')
+            if iteration == 1:          
+                spectrograms = np.concatenate(spectrograms, axis=1)
+                # write spectrograms
+                write_dataset(spectrograms,
+                            output_data_dir=output_pickle_dir,
+                            dataset_type=dataset_type,
+                            suffix='frames')
 
-            #     del spectrograms
+                del spectrograms
 
 
 
